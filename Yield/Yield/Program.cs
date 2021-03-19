525fced2025f9e2325716aff8c7d23213d4dae99
castanc <54039892+castanc@users.noreply.github.com>
2021-03-19 10:27:36 -0300

    Update Program.cs

--- Yield/Yield/Program.cs
+++ Yield/Yield/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Yield
@@ -9,18 +9,21 @@ namespace Yield
         {
             foreach(var oElement in GetNumeros())
             {
-                Console.WriteLine("Se solicita archivo grande");
+                Console.WriteLine($"Se solicita archivo grande: {oElement} ");
             }
+            Console.ReadLine();
         }
 
         static IEnumerable<int> GetNumeros()
         {
+            List<int> list = new List<int>();
             for (int i = 1; i < 11; i++)
             {
-                Console.WriteLine("Leo un archivo grande de 1GB");
-                yield return i;
-                Console.WriteLine("Liberamos memoria");
+                Console.WriteLine($"Leo un archivo grande de 1GB: {i}");
+                //yield return i;
+                list.Add(i);
             }
+            return list;
         }
     }
 }
