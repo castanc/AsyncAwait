c05c86adf6096b7bcf77f0a8a144cdaad510617d
castanc <54039892+castanc@users.noreply.github.com>
2021-03-18 22:52:21 -0300

    Add files via upload

---
+++ Yield/Yield/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var oElement in GetNumeros())
+            {
+                Console.WriteLine("Se solicita archivo grande");
+            }
+        }
+
+        static IEnumerable<int> GetNumeros()
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine("Leo un archivo grande de 1GB");
+                yield return i;
+                Console.WriteLine("Liberamos memoria");
+            }
+        }
+    }
+}
