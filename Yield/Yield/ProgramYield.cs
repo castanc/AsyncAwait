4407f3f07636166ef067b7df16fa737ace9f53e3
castanc <54039892+castanc@users.noreply.github.com>
2021-03-19 10:28:21 -0300

    Create ProgramYield.cs

---
+++ Yield/Yield/ProgramYield.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+
+namespace Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var oElement in GetNumeros())
+            {
+                Console.WriteLine($"Se solicita archivo grande: {oElement} ");
+            }
+            Console.ReadLine();
+        }
+
+        static IEnumerable<int> GetNumeros()
+        {
+            List<int> list = new List<int>();
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"Leo un archivo grande de 1GB: {i}");
+                yield return i;
+                list.Add(i);
+            }
+            //return list;
+        }
+    }
+}
